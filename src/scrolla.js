b4c728ae81f45a9437e2611c941b06b2e5d95403
Ron Valstar <sjeiti@gmail.com>
2015-07-07 20:06:25 +0200

    updated bower ignore list

--- src/scrolla.js
+++ src/scrolla.js
@@ -1,6 +1,6 @@
 /**
  * Scrolla is a pure-js, cross-everything scrollbar script.
- * @version 0.1.1
+ * @version 0.1.2
  * @license MIT/GPL
  * @author Ron Valstar <ron@ronvalstar.nl>
  * @copyright Ron Valstar <ron@ronvalstar.nl>
